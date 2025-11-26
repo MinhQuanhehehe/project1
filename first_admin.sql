c3b30a99c9e420d1105c19380fd517e6be84fb06
bachnlp <bachtvvnuis@gmail.com>
2025-11-27 01:58:53 +0700

    Add files via upload
    - Admin account can be created by query.
    - Password of Admin hashed thru PHP script.
    - status as 'active' by default.

---
+++ first_admin.sql
@@ -0,0 +1,4 @@
+INSERT INTO user
+	(Username, Password, FullName, Age, Role, Status)
+VALUES
+	('ADMIN', '$2y$10$/OTlGPL.OHpNNgyrBjHeQOc1Pbr.nYFEEr6f4/ZBMelYd887UQXDG', 'administrator', 20, 'admin', 'active')
