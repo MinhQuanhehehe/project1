37315b19a6c4fef2ecf854e98dacd47ff7c0fc5e
MinhQuanhehehe <22071057@vnu.edu.vn>
2025-12-02 10:57:01 +0700

    rrefactorring file

--- todo_app_db.sql
+++ todo_app_db.sql
@@ -35,4 +35,8 @@ CREATE TABLE IF NOT EXISTS Task (
                                    FOREIGN KEY (UserID) REFERENCES User(UserID) ON DELETE CASCADE,
                                    FOREIGN KEY (ListID) REFERENCES List(ListID) ON DELETE SET NULL
 );
+INSERT INTO user
+(Username, Password, FullName, Age, Role, Status)
+VALUES
+    ('ADMIN', '$2y$10$/OTlGPL.OHpNNgyrBjHeQOc1Pbr.nYFEEr6f4/ZBMelYd887UQXDG', 'administrator', 20, 'admin', 'active')
 
