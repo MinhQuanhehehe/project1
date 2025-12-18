b6047e835289e39c8dfc82088226556fa5a42b68
bachnlp <bachtvvnuis@gmail.com>
2025-12-18 16:27:20 +0700

    Update login.php

--- todolist/auth/login.php
+++ todolist/auth/login.php
@@ -58,7 +58,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Todo App Pro</title>
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css?v=1.0">
 </head>
 <body>
 <div class="container auth-container">
